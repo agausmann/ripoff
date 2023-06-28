73ec3a6c07f848cee9e3d037a187d97bb5ff9b59
Adam Gausmann <adam@gaussian.dev>
2023-06-27 20:50:32 -0500

    Switch to ureq and lookup discid results

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,28 @@ pub struct Cli {
 fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
 
-    let disc_id = DiscId::read(args.disc_device.as_deref())?;
+    let disc_info = DiscId::read(args.disc_device.as_deref())?;
+    let disc_id = disc_info.id();
+    let toc = disc_info.toc_string();
 
-    println!("{:?}", disc_id.id());
+    println!("Disc ID: {:?}", disc_id);
+    println!("TOC: {:?}", toc);
+
+    const USER_AGENT: &str = concat!(
+        env!("CARGO_PKG_NAME"),
+        "/",
+        env!("CARGO_PKG_VERSION"),
+        " ( ",
+        env!("CARGO_PKG_HOMEPAGE"),
+        " )",
+    );
+    let response = ureq::get(&format!("https://musicbrainz.org/ws/2/discid/{}", disc_id))
+        .set("User-Agent", USER_AGENT)
+        .set("Accept", "application/json")
+        .call()?
+        .into_string()?;
+
+    println!("{}", response);
 
     Ok(())
 }
